18343bea505da9c6cb0b71348e34768ad0da2e86
Marcin Konwiak <marcin2899@gmail.com>
2025-04-16 23:26:10 +0200

    Initialize React client

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        className="
+           bg-blue-100
+           text-blue-800
+           font-semibold
+           py-2
+           px-4
+           rounded-full
+           hover:bg-blue-200
+           focus:outline-none
+           focus:ring-2
+           focus:ring-blue-500
+           transition
+           duration-300
+           ease-in-out
+           m-4
+           shadow-md
+         "
+      >
+        Hello, Tailwind CSS!
+      </div>
+    </>
+  );
+}
+
+export default App;
